6ce9c84781ee30a195ac9472edb8dfd10fb722c9
Abraao <aohdigital@hotmail.com>
2023-05-03 17:45:42 -0300

    Dark Mode

--- src/app/pokedex.service.ts
+++ src/app/pokedex.service.ts
@@ -20,7 +20,5 @@ export class PokedexService {
 }
 
 
-
-
 }
 
--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,53 +1,56 @@
 import { Component } from '@angular/core';
-import {  PokedexService } from '../pokedex.service';
+import { PokedexService } from '../pokedex.service';
 import { Dex } from '../Dex';
 
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.component.html',
-  styleUrls: ['./pokedex.component.css']
+  styleUrls: ['./pokedex.component.css'],
 })
 export class PokedexComponent {
+  Dex: Dex = {} as Dex;
 
+  constructor(private service: PokedexService) {}
 
-Dex : Dex = {} as Dex;
-
-constructor( private service :  PokedexService){}
-
-
-
-loadPokemon() {
-  this.service.getPokemon().subscribe(
-    {
-      next: data => {
+  loadPokemon() {
+    this.service.getPokemon().subscribe({
+      next: (data) => {
         this.Dex = data;
         this.Dex.img = data.sprites.other.home.front_default;
-      }
-    }
-  );
-}
-loadNextPokemon() {
-  this.service.id = this.service.id + 1;
-  this.service.getPokemon().subscribe(
-    {
-      next: data => {
+      },
+    });
+  }
+  loadNextPokemon() {
+    this.service.id = this.service.id + 1;
+    this.service.getPokemon().subscribe({
+      next: (data) => {
         this.Dex = data;
         this.Dex.img = data.sprites.other.home.front_default;
-      }
-    }
-  );
-}
+      },
+    });
+  }
 
-loadBackPokemon() {
-  this.service.id = this.service.id - 1;
-  this.service.getPokemon().subscribe(
-    {
-      next: data => {
+  loadBackPokemon() {
+    this.service.id = this.service.id - 1;
+    this.service.getPokemon().subscribe({
+      next: (data) => {
         this.Dex = data;
         this.Dex.img = data.sprites.other.home.front_default;
-      }
-    }
-  );
-}
+      },
+    });
+  }
 
+  loadRandom() {
+    this.service.id = Math.floor(Math.random() * 1008) + 1;
+    this.service.getPokemon().subscribe({
+      next: (data) => {
+        this.Dex = data;
+        this.Dex.img = data.sprites.other.home.front_default;
+      },
+    });
+  }
+  toggleDarkMode() {
+    const body = document.getElementsByName('TT')[0];
+    body.classList.toggle('dark-mode');
+  }
 }
